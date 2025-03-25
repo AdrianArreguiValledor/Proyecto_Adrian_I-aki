cf9cb21ae1669b018b7dd93dc803313706452b7f
AdrianArreguiValledor <adrian.arregui@sm-services.es>
2025-03-25 10:58:33 +0100

    Add files via upload

---
+++ proyecto_tienda/src/main/java/com/formacion/proyecto_tienda/ProyectoTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.proyecto_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTiendaApplication.class, args);
+	}
+
+}
